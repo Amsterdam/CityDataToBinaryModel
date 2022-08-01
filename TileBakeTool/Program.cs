b38fc39f1f4a8425ea1f8a4a21feb13a96e9dad1
Sammey <samuelpetrus@gmail.com>
2022-08-01 09:00:11 +0200

    reformatting

--- TileBakeTool/Program.cs
+++ TileBakeTool/Program.cs
@@ -24,16 +24,16 @@ using TileBakeLibrary;
 
 namespace TileBakeTool
 {
-	class Program
-	{
-		private static ConfigFile configFile;
+    class Program
+    {
+        private static ConfigFile configFile;
 
-		private static string sourcePathOverride = "";
-		private static string outputPathOverride = "";
-		private static float lodOverride = 1;
+        private static string sourcePathOverride = "";
+        private static string outputPathOverride = "";
+        private static float lodOverride = 1;
 
-		static void Main(string[] args)
-		{
+        static void Main(string[] args)
+        {
             Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
 
             //No parameters or an attempt to call for help? Show help in console.
@@ -46,7 +46,7 @@ namespace TileBakeTool
             {
                 ApplyConfigFileSettings(args[0]);
             }
-             //More parameters? Parse them
+            //More parameters? Parse them
             else
             {
                 ParseArguments(args);
@@ -57,98 +57,101 @@ namespace TileBakeTool
                 StartConverting();
         }
 
-		private static void ParseArguments(string[] args)
-		{
-			//Read the arguments and apply corresponding settings
-			for (int i = 0; i < args.Length; i++)
-			{
-				var argument = args[i];
-				if (argument.Contains("--")){
-					var value = (i + 1 < args.Length) ? args[i + 1] : "";
-					ApplySetting(argument,value);
-				}
-			}
-		}
+        private static void ParseArguments(string[] args)
+        {
+            //Read the arguments and apply corresponding settings
+            for (int i = 0; i < args.Length; i++)
+            {
+                var argument = args[i];
+                if (argument.Contains("--"))
+                {
+                    var value = (i + 1 < args.Length) ? args[i + 1] : "";
+                    ApplySetting(argument, value);
+                }
+            }
+        }
 
-		/// <summary>
-		/// Load a .json config file and apply it to our settings object
-		/// </summary>
-		/// <param name="configFilePath">Path to config file</param>
-		private static void ApplyConfigFileSettings(string configFilePath){
-			if(File.Exists(configFilePath))
-			{
-				var configJsonText = File.ReadAllText(configFilePath);
-				configFile = JsonSerializer.Deserialize<ConfigFile>(configJsonText
-				, new JsonSerializerOptions()
-				{ 
-					AllowTrailingCommas = true }
-				);
-				Console.WriteLine($"Loaded config file: {Path.GetFileName(configFilePath)}");
-			}
-		}
-
-
-		/// <summary>
-		/// Apply commandline parameters as settings
-		/// </summary>
-		/// <param name="argument">Commandline parameter argument</param>
-		/// <param name="value">Commandline parameter value</param>
-		private static void ApplySetting(string argument, string value)
-		{
-			switch (argument)
-			{
-				case "--config":
-					ApplyConfigFileSettings(value);
-					break;
-				case "--source":
-					sourcePathOverride = value;
-					Console.WriteLine($"Source: {value}");
-					break;
-				case "--output":
-					outputPathOverride = value;
-					Console.WriteLine($"Output directory: {value}");
-					break;
-				case "--lod":
-					lodOverride = float.Parse(value,System.Globalization.CultureInfo.InvariantCulture);
-					Console.WriteLine($"LOD filter: {lodOverride}");
-					break;
-				default:
-					break;
-			}
-		}
-
-		/// <summary>
-		/// Start the converting process using the current configuration
-		/// </summary>
-		private static void StartConverting()
-		{
-			Console.WriteLine("Starting...");
-
-			//Here we use the .dll. This way we may use this library in Unity, or an Azure C# Function
-			var tileBaker = new CityJSONToTileConverter();
-			tileBaker.SetSourcePath((sourcePathOverride != "") ? sourcePathOverride : configFile.sourceFolder);
-			tileBaker.SetTargetPath((outputPathOverride != "") ? outputPathOverride : configFile.outputFolder);
-			tileBaker.SetLOD((lodOverride != 1) ? lodOverride : configFile.lod);
-			tileBaker.SetVertexMergeAngleThreshold(configFile.mergeVerticesBelowAngle);
-			tileBaker.SetID(configFile.identifier, configFile.removePartOfIdentifier);
-			tileBaker.SetReplace(configFile.replaceExistingObjects);
-			tileBaker.SetExportUV(configFile.exportUVCoordinates);
-			tileBaker.AddBrotliCompressedFile(configFile.brotliCompression);
-			tileBaker.SetClipSpikes(configFile.removeSpikes, configFile.removeSpikesAbove, configFile.removeSpikesBelow);
-			tileBaker.SetObjectFilters(configFile.cityObjectFilters);
-			tileBaker.SetTileSize(configFile.tileSize);
-			tileBaker.TilingMethod = configFile.tilingMethod;
-
-			tileBaker.Convert();
-		}
+        /// <summary>
+        /// Load a .json config file and apply it to our settings object
+        /// </summary>
+        /// <param name="configFilePath">Path to config file</param>
+        private static void ApplyConfigFileSettings(string configFilePath)
+        {
+            if (File.Exists(configFilePath))
+            {
+                var configJsonText = File.ReadAllText(configFilePath);
+                configFile = JsonSerializer.Deserialize<ConfigFile>(configJsonText
+                , new JsonSerializerOptions()
+                {
+                    AllowTrailingCommas = true
+                }
+                );
+                Console.WriteLine($"Loaded config file: {Path.GetFileName(configFilePath)}");
+            }
+        }
 
 
-		/// <summary>
-		/// Draw the help text in the commandline
-		/// </summary>
-		private static void ShowHelp()
-		{
-			Console.Write(Constants.helpText);
-		}
-	}
+        /// <summary>
+        /// Apply commandline parameters as settings
+        /// </summary>
+        /// <param name="argument">Commandline parameter argument</param>
+        /// <param name="value">Commandline parameter value</param>
+        private static void ApplySetting(string argument, string value)
+        {
+            switch (argument)
+            {
+                case "--config":
+                    ApplyConfigFileSettings(value);
+                    break;
+                case "--source":
+                    sourcePathOverride = value;
+                    Console.WriteLine($"Source: {value}");
+                    break;
+                case "--output":
+                    outputPathOverride = value;
+                    Console.WriteLine($"Output directory: {value}");
+                    break;
+                case "--lod":
+                    lodOverride = float.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
+                    Console.WriteLine($"LOD filter: {lodOverride}");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Start the converting process using the current configuration
+        /// </summary>
+        private static void StartConverting()
+        {
+            Console.WriteLine("Starting...");
+
+            //Here we use the .dll. This way we may use this library in Unity, or an Azure C# Function
+            var tileBaker = new CityJSONToTileConverter();
+            tileBaker.SetSourcePath((sourcePathOverride != "") ? sourcePathOverride : configFile.sourceFolder);
+            tileBaker.SetTargetPath((outputPathOverride != "") ? outputPathOverride : configFile.outputFolder);
+            tileBaker.SetLOD((lodOverride != 1) ? lodOverride : configFile.lod);
+            tileBaker.SetVertexMergeAngleThreshold(configFile.mergeVerticesBelowAngle);
+            tileBaker.SetID(configFile.identifier, configFile.removePartOfIdentifier);
+            tileBaker.SetReplace(configFile.replaceExistingObjects);
+            tileBaker.SetExportUV(configFile.exportUVCoordinates);
+            tileBaker.AddBrotliCompressedFile(configFile.brotliCompression);
+            tileBaker.SetClipSpikes(configFile.removeSpikes, configFile.removeSpikesAbove, configFile.removeSpikesBelow);
+            tileBaker.SetObjectFilters(configFile.cityObjectFilters);
+            tileBaker.SetTileSize(configFile.tileSize);
+            tileBaker.TilingMethod = configFile.tilingMethod;
+
+            tileBaker.Convert();
+        }
+
+
+        /// <summary>
+        /// Draw the help text in the commandline
+        /// </summary>
+        private static void ShowHelp()
+        {
+            Console.Write(Constants.helpText);
+        }
+    }
 }
